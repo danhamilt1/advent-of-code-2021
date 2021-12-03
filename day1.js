f69e1f22ae94b01321439fe7dafa915fc7eadb65
danhamilt1 <danhamilt1@gmail.com>
2021-12-03 08:37:43 +0000

    day1

---
+++ day1.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+const day1 = () => {
+    let data;
+    try {
+        data = fs.readFileSync('inputs/day_1.txt', 'utf8');
+    } catch (err) {
+        console.error(err)
+    }
+
+    const increases = data
+        .split('\n')
+        .map(item => Number(item))
+        .reduce((agg, current, idx) => {
+            if(idx > 0 && current > depths[idx-1]) {
+                agg += 1;
+            }
+            return agg;
+        }, 0);
+
+    console.log(increases);
+}
+
+export default day1
